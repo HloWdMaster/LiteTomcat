ca16891649681b8c10f3db4e1b47dbcc92e29fe3
MinNong <1457391646@qq.com>
2019-05-07 00:21:18 +0800

    第二章节:servlet容器

---
+++ src/main/java/ex02/HttpServer1.java
@@ -0,0 +1,10 @@
+package ex02;
+
+/**
+ * Create by leonardo on 2019/5/6
+ */
+public class HttpServer1 {
+
+    private static final String SHUTDOWN_COMMAND = "/SHUTDOWN";
+
+}
---
+++ src/main/java/ex02/PrimitiveServlet.java
@@ -0,0 +1,38 @@
+package ex02;
+
+import ex01.Response;
+
+import javax.servlet.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Create by leonardo on 2019/5/6
+ */
+public class PrimitiveServlet implements Servlet {
+
+
+    public void init(ServletConfig servletConfig) throws ServletException {
+        System.out.println("init");
+    }
+
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println("from service");
+        PrintWriter out = servletResponse.getWriter();
+        out.println("Hello. Rose are red.");
+        out.print("Violets are blue");
+
+    }
+
+    public String getServletInfo() {
+        return null;
+    }
+
+    public void destroy() {
+        System.out.println("destory");
+    }
+}
